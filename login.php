577df4c981ab93735cfa9221692294ebf40e2970
srjobinar <jobinar007@gmail.com>
2016-04-22 01:23:38 +0530

    fix

--- login.php
+++ login.php
@@ -14,31 +14,9 @@ require './classes/boot.php';
 require_once './classes/session.php';
 
 
-use Illuminate\Validation\Factory as ValidatorFactory;
-use Symfony\Component\Translation\Translator;
-
-$error = 0;
-const ERROR_DB = 1;
-const ERROR_INVALID = 3;
-const ERROR_VALFAIL = 4;
-const ERROR_MAILTAKEN = 5;
-
-
-$session = new Session();
-$session->redirectIfAuth('./admin/index.php','./trainer/index.php','./doctor/index.php','./index.php');
-
-$factory = new ValidatorFactory(new Translator('en'));
-
-if(isset($_POST['type'])){
-  $type = $_POST['type'];
-
-  if($type=="login"){
-
-    $validator = $factory->make($_POST, ['phone'=>'required','password'=>'required'],$messages);
-
-    if($validator->passes()) {
-      $u = User::where('phone',$_POST['phone'])->where('password',$_POST['password'])->first();
-      if($u){
+$u = User::where('phone',$_POST['phone'])->where('password',$_POST['password'])->first();
+      
+if($u){
 
         $session->logIn($u->id, $u->type);
         $out['status'] = "success";
@@ -48,45 +26,7 @@ if(isset($_POST['type'])){
         $out['error'] = "invalid credentials.";
 
       }
-    }else{
-        $out['status'] = "fail";
-        $out['error'] = "phone/password missing in data.";
-    }
-
-  }else if($type=="signup"){
-    $rules = array(
-      'name' => 'required',
-      'email' => 'required|email',
-      'phone' => 'required|numeric|digits_between:10,15',
-      'address' =>'required',
-      'password' => 'required'
-    );
-    $validator = $factory->make($_POST, $rules,$messages);
-    if($validator->passes()) {
-      $_POST['type'] = Session::USER_REGULAR;
-      //check if email exits
-      $u = User::where('email',$_POST['email'])->first();
-      if($u){
-        $error = ERROR_DB;
-      }else{
-        if($u = User::create($_POST)){
-
-        $session->logIn($u->id, $u->type);
-
-        }else{
-          $error = ERROR_DB;
-        }
-      }
-    }else{
-      $error = ERROR_VALFAIL;
-    }
-  }
-}else{
- //type not set
- $out['status'] = "fail";
- $out['error'] = "type not set.";
-}
-
+    
 echo json_encode($out,TRUE);
 
  ?>
